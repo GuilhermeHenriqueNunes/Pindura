f4606809a34673e81497f0d0cf8aef612323cf78
Guilherme Nunes <guilhermehenriquenunes21@gmail.com>
2026-09-17 20:41:45 -0300

    Criando o Projeto

---
+++ src/main/java/Projeto/Pindura/PinduraApplication.java
@@ -0,0 +1,13 @@
+package Projeto.Pindura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinduraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinduraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto/Pindura/PinduraApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.Pindura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinduraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
